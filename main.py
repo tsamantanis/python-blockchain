249ba56711406efcd713a2cbed94737839c38eaa
tsamantanis <philip.tsamantanis@icloud.com>
2021-06-17 07:14:08 -0700

    Initial setup

---
+++ main.py
@@ -0,0 +1,22 @@
+def __init__(self):
+    """Initialize blockchain"""
+    self.chain = []
+    self.current_transactions = []
+
+
+def new_block(self):
+    """Create new blocks and then add them to existing chain"""
+    pass
+
+def new_transaction(self):
+    """Add a new transaction to already existing transactions"""
+    pass
+
+@staticmethod
+def hash(block):
+    """Hash function for blocks"""
+
+@property
+def last_block(self):
+    """Return the last block of the chain"""
+    pass
