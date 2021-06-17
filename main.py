64d87b685b1a038eb431e1835a1f3e21f84f1829
tsamantanis <philip.tsamantanis@icloud.com>
2021-06-17 07:17:16 -0700

    Add basic functionality

--- main.py
+++ main.py
@@ -2,21 +2,38 @@ def __init__(self):
     """Initialize blockchain"""
     self.chain = []
     self.current_transactions = []
+    self.new_block(previous_hash=1, proof=100)
 
-
-def new_block(self):
+def new_block(self, proof, previous_hash = None):
     """Create new blocks and then add them to existing chain"""
-    pass
+    block = {
+       'index': len(self.chain) + 1,
+       'timestamp' : time(),
+       'proof': proof,
+       previous_hash: previous_hash or self.hash(self.chain[-1]),
+    }
+    self.current_transactions = []
+    self.chain.append(block)
+    return block
+
 
 def new_transaction(self):
     """Add a new transaction to already existing transactions"""
-    pass
+    self.current_transactions.append({
+        'sender': sender,
+        'recipient': recipient,
+        'amount': amount,
+    })
+    return self.last_block['index'] + 1
 
 @staticmethod
 def hash(block):
-    """Hash function for blocks"""
+    """Hash function for blocks"""\
+    block_string = json.dumps(block, sort_keys = True).encode()
+    return hashlib.sha256(block_string).hexdigest()
 
 @property
 def last_block(self):
     """Return the last block of the chain"""
-    pass
+    return self.chain[-1]
+
