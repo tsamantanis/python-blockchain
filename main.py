a10e6819b0686295c93da0555a1cdd16db3c1b5b
tsamantanis <philip.tsamantanis@icloud.com>
2021-06-17 07:18:38 -0700

    Implement proof of work

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ def __init__(self):
     self.current_transactions = []
     self.new_block(previous_hash=1, proof=100)
 
+def proof_of_work(self, last_proof):
+   """Issuing proof of work"""
+   proof = 0
+   while self.valid_proof(last_proof, proof) is False:
+       proof +=1
+   return proof
+
+@staticmethod
+def valid_proof(last_proof, proof):
+   """Validate block"""
+   guess = f'{last_proof}{proof}'.encode()
+   guess_hash = hashlib.sha256(guess).hexigest()
+   return guess_hash[:4] == "0000"
+   
 def new_block(self, proof, previous_hash = None):
     """Create new blocks and then add them to existing chain"""
     block = {
@@ -36,4 +50,3 @@ def hash(block):
 def last_block(self):
     """Return the last block of the chain"""
     return self.chain[-1]
-
